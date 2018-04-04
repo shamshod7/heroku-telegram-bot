addcfb245407bbe537a392a930ef7def357a1a9b
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-04 23:02:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ def size(m):
         sredn=x['summ']/x['kolvo']
     except:
         sredn=0
-    bot.send_message(m.from_user.id, m.from_user.first_name+', средний размер вашего члена: '+str(sredn)+' см.\nВы измеряли член '+str(x['kolvo'])+' раз!') 
+    bot.send_message(m.from_user.id, m.from_user.first_name+', средний размер вашего члена: '+str(sredn)+' см.\nВы измеряли член '+str(x['kolvo'])+' раз(а)!') 
                     
                                                                                                                                          
                                                                                                                                          
