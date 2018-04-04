c6698cf355893cb8f0581b8adece25e612e72c53
egor5q <31898567+egor5q@users.noreply.github.com>
2018-04-04 22:53:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -149,8 +149,8 @@ def chlenomer(message):
             bot.send_message(message.chat.id, replytext)
             x=iduser.find_one({'id':message.from_user.id})
             otvet=chlen+mm/100
-            iduser.update_one({x}, {'$inc':{'kolvo':1}})
-            iduser.update_one({x}, {'$inc':{'summ':otvet}})
+            iduser.update_one({x}, {'$inc':{'x.kolvo':1}})
+            iduser.update_one({x}, {'$inc':{'x.summ':otvet}})
         if mega==1:
             text='Вы нашли секретное сообщение, шанс которого 1%!'+"\n"+'Есть еще секретные сообщения, шанс которых еще ниже...'
             t=1
