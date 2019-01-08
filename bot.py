820d7d2592e88af08ac8bf55cebe58e5ed664330
egor5q <31898567+egor5q@users.noreply.github.com>
2019-01-08 21:33:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,8 +48,8 @@ def donatemes(m):
 @bot.message_handler(commands=['removedailyuser'])
 def removedailyu(m):
     pass
-    #x=get_chat_member(m.chat.id, m.from_user.id)
-    #print(x)
+    x=bot.get_chat_administrators(m.chat.id)
+    print(x)
     #tr=0
     #for ids in x:
     #    print(ids)
